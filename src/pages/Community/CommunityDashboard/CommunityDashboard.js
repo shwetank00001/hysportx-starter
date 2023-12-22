af8524d50f9b3b40e9210356b1e2ebff99886ddf
shwetank00001 <mshwetank@gmail.com>
2023-12-22 20:33:08 +0530

    all working

--- src/pages/Community/CommunityDashboard/CommunityDashboard.js
+++ src/pages/Community/CommunityDashboard/CommunityDashboard.js
@@ -1,8 +1,27 @@
 import React from 'react'
 
 const CommunityDashboard = () => {
+    document.title = "Dashboard | Skote - React Admin & Dashboard Template"
+
+    const [roleType, setRoleType] = React.useState([])
+
+    React.useEffect(() => {
+      const userData = JSON.parse(localStorage.getItem("userData"))
+      setRoleType(userData.role.type)
+    }, [])
+
+    console.log("Admin dash", roleType)
+
+    if (roleType !== "Admin") {
+      const nav = useNavigate()
+      nav("/error")
+    }
   return (
-    <div>CommunityDashboard</div>
+    <React.Fragment>
+      <div className="page-content">
+        <p>Community Dashboard</p>
+      </div>
+    </React.Fragment>
   )
 }
 
