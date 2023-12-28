7e7ec43d841d60b52a474eaf64fb37c502f711f0
shwetank00001 <mshwetank@gmail.com>
2023-12-28 14:42:52 +0530

    added participator

--- src/pages/Community/ParticipatorList/participator.js
+++ src/pages/Community/ParticipatorList/participator.js
@@ -1,95 +1,66 @@
-import React, { useState, useMemo, useEffect } from "react"
+import React, { useState, useEffect, useMemo } from "react"
+import { Link } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
-import { Link, useNavigate } from "react-router-dom"
+import * as Yup from "yup"
+import { useFormik } from "formik"
 import {
   Col,
-  Input,
   Row,
-  Label,
-  Modal,
-  Form,
   Card,
   CardBody,
-  Button,
+  Label,
+  Form,
+  Input,
+  Modal,
+  ModalHeader,
+  ModalBody,
   UncontrolledTooltip,
-  Container,
+  Button,
 } from "reactstrap"
 import TableContainer from "components/Common/TableContainer"
-//import components
-import Breadcrumbs from 'components/Common/Breadcrumb';
-
-import { participatorListRequest } from '../../../store/participator/actions'
-
-
-const participatorList = [
-  {
-    id: 1,
-    index: 1,
-    firstName: "Jennifer Chang",
-    lastName: "Jennifer Chang",
-    email: "shwetank",
-    mobile: "88888888888",
-  },
-  {
-    id: 1,
-    index: 2,
-    firstName: "Jennifer Chang",
-    lastName: "Jennifer Chang",
-    email: "shwetank",
-    mobile: "88888888888",
-  },
-  {
-    id: 1,
-    index: 3,
-    firstName: "Jennifer Chang",
-    lastName: "Jennifer Chang",
-    email: "shwetank",
-    mobile: "88888888888",
-  },
-  {
-    id: 1,
-    index: 4,
-    firstName: "Jennifer Chang",
-    lastName: "Jennifer Chang",
-    email: "shwetank",
-    mobile: "88888888888",
-  },
-  {
-    id: 1,
-    index: 5,
-    firstName: "Jennifer Chang",
-    lastName: "Jennifer Chang",
-    email: "shwetank",
-    mobile: "88888888888",
-  },
-]
-
-
+import Spinners from "components/Common/Spinner"
+import Breadcrumbs from "components/Common/Breadcrumb"
+import DeleteModal from "components/Common/DeleteModal"
+import { ToastContainer } from "react-toastify"
+import { createSelector } from "reselect"
+import {
+  participatorListRequest,
+  deleteParticipatorRequest,
+} from "../../../store/participator/actions"
 
 const Participator = () => {
-
   const dispatch = useDispatch()
-  const parti = useSelector(state => state.participatorReducer.participator)
-  console.log("participator data", parti)
+  const [modal, setModal] = useState(false)
 
-  useEffect( () => {
-    dispatch(participatorListRequest())
+  const fetchParticipatorList = state => state.participatorReducer
+  const ParticipatorDataProperties = createSelector(
+    fetchParticipatorList,
+    participatorReducer => ({
+      Participator: participatorReducer.participator,
+      loading: participatorReducer.loading,
+    })
+  )
+
+  const { Participator, loading } = useSelector(ParticipatorDataProperties)
+  const [isLoading, setLoading] = useState(loading)
+
+  useEffect(() => {
+    const fetchData = () => {
+      setLoading(true)
+      dispatch(participatorListRequest())
+      setTimeout(() => {
+        setLoading(false)
+      }, 1000)
+    }
+    fetchData()
   }, [dispatch])
 
+  console.log("Participator here", Participator.requests)
 
-
-
-  const [open, setOpen] = useState(false)
-
-  function removeBodyCss() {
-    document.body.classList.add("no_padding")
-  }
-
-  //coloums header start
   const columns = useMemo(
     () => [
       {
-        Header: "Sr No",
+        Header: "No",
         accessor: "id",
         Cell: cellProps => {
           return (
@@ -99,25 +70,22 @@ const Participator = () => {
           )
         },
       },
-
       {
-        Header: "First Name",
-        accessor: "firstName",
+        Header: "Participator Name",
+        accessor: "first_name",
       },
       {
         Header: "Last Name",
-        accessor: "lastName",
+        accessor: "last_name",
       },
-
       {
         Header: "Email",
         accessor: "email",
       },
       {
-        Header: "Mobile No",
-        accessor: "mobile",
+        Header: "Phone",
+        accessor: "phone",
       },
-
       {
         Header: "Action",
         accessor: "action",
@@ -128,7 +96,12 @@ const Participator = () => {
               <li data-bs-toggle="tooltip" data-bs-placement="top" title="View">
                 <Button
                   className="btn btn-sm btn-soft-primary"
-                  id={`viewtooltip-${cellProps.row.original.id}`} //use param Hook
+                  id={`viewtooltip-${cellProps.row.original.id}`}
+                  onClick={() => {
+                    const participator_id = cellProps.row.original
+                    onClickView(participator_id)
+                    setModal(true)
+                  }}
                 >
                   <i className="mdi mdi-eye-outline" />
                 </Button>
@@ -139,7 +112,6 @@ const Participator = () => {
               >
                 View
               </UncontrolledTooltip>
-
               <li>
                 <Button
                   className="btn btn-sm btn-soft-primary"
@@ -154,15 +126,14 @@ const Participator = () => {
                   </UncontrolledTooltip>
                 </Button>
               </li>
-
               <li>
                 <Link
                   to="#"
+                  onClick={() => {
+                    const participator_id = cellProps.row.original
+                    onClickDelete(participator_id)
+                  }}
                   className="btn btn-sm btn-soft-danger"
-                  // onClick={() => {
-                  //     const jobData = cellProps.row.original;
-                  //     onClickDelete(jobData);
-                  // }}
                   id={`deletetooltip-${cellProps.row.original.id}`}
                 >
                   <i className="mdi mdi-delete-outline" />
@@ -179,173 +150,217 @@ const Participator = () => {
         },
       },
     ],
-
     []
   )
+
+  const [deleteModal, setDeleteModal] = useState(false)
+  const [participator, setParticipator] = useState(null)
+
+  const onClickDelete = participator => {
+    setParticipator(participator)
+    setDeleteModal(true)
+  }
+
+  const handleDeleteParticipator = () => {
+    if (participator && participator.id) {
+      setLoading(true)
+      dispatch(deleteParticipatorRequest(participator.id))
+      setDeleteModal(false)
+      setTimeout(() => {
+        dispatch(participatorListRequest())
+        setLoading(false)
+      }, 1000)
+    }
+  }
+
+  const modalities = data => {
+    const value = data.map(item => {
+      return item.name
+    })
+    const commaSeparatedString = value.join(",")
+    return commaSeparatedString
+  }
+
+  const [viewparticipatordata, setParticipatorData] = useState(null)
+
+  const onClickView = item => {
+    setParticipatorData(item)
+  }
+
+  const validation = useFormik({
+    enableReinitialize: true,
+
+    initialValues: {
+      participator_name:
+        (viewparticipatordata && viewparticipatordata.name) || "",
+      participator_level:
+        (viewparticipatordata && viewparticipatordata.level) || "",
+      participator_modality:
+        (viewparticipatordata && modalities(viewparticipatordata.modalities)) ||
+        "",
+    },
+    validationSchema: Yup.object({
+      participator_name: Yup.string().required(
+        "Please Enter Participator Name"
+      ),
+    }),
+    onSubmit: values => {},
+  })
+
   return (
     <React.Fragment>
-      <Container fluid>
-        {/* Render Breadcrumbs */}
-        <Breadcrumbs title="fwgames" breadcrumbItem="hyposports" />
-        
-        <Card>
-
-          <Col xl={12}>
-            <Card>
-              <CardBody className="border-bottom">
-                <div className="d-flex align-items-center">
-                  <h5 className="mb-0 card-title flex-grow-1">Participator Lists</h5>
-                  <div className="flex-shrink-0">
-                    <Link to="#!" className="btn btn-light me-1"><i className="mdi mdi-refresh"></i></Link>
-                    <button className="btn btn-primary" onClick={() => {setOpen(!open), removeBodyCss() }}> <i className="mdi mdi-plus me-1" />Create Participator</button>
-
+      <DeleteModal
+        show={deleteModal}
+        onDeleteClick={handleDeleteParticipator}
+        onCloseClick={() => setDeleteModal(false)}
+      />
+      {/*  Modal view and edit */}
+      <div className="page-content">
+        <div className="container-fluid">
+          <Breadcrumbs title="" breadcrumbItem="Participator List" />
+          <Row>
+            <Col lg="12">
+              <Card>
+                <CardBody className="border-bottom">
+                  <div className="d-flex align-items-center">
+                    <h5 className="mb-0 card-title flex-grow-1">
+                      Participator Lists
+                    </h5>
+                    <div className="flex-shrink-0">
+                      <Link to="#!" className="btn btn-light me-1">
+                        <i className="mdi mdi-refresh"></i>
+                      </Link>
+                      <button className="btn btn-primary">
+                        {" "}
+                        <i className="mdi mdi-plus me-1" />
+                        Create Participator
+                      </button>
+                    </div>
+                  </div>
+                </CardBody>
+                {isLoading ? (
+                  <Spinners setLoading={setLoading} />
+                ) : (
+                  <CardBody>
+                    <TableContainer
+                      columns={columns}
+                      data={
+                        Participator.requests ? Participator.requests : [{}]
+                      }
+                      isGlobalFilter={true}
+                      isPagination={true}
+                      iscustomPageSizeOptions={true}
+                      isShowingPageLength={true}
+                      customPageSize={5}
+                      tableClass="table-bordered align-middle nowrap mt-2"
+                      paginationDiv="col-sm-12 col-md-7"
+                      pagination="pagination justify-content-end pagination-rounded"
+                    />
+                  </CardBody>
+                )}
+              </Card>
+            </Col>
+          </Row>
+          <ToastContainer />
+        </div>
+      </div>
+      <Modal
+        isOpen={modal}
+        toggle={() => {
+          setModal()
+        }}
+        id="participator"
+      >
+        <div className="modal-content">
+          <ModalHeader
+            toggle={() => setModal()}
+            id="participatorLabel"
+            className="modal-header"
+          >
+            View Participator
+          </ModalHeader>
+          <ModalBody>
+            <Form
+              className="form-horizontal"
+              onSubmit={e => {
+                e.preventDefault()
+                validation.handleSubmit()
+                return false
+              }}
+            >
+              <Row>
+                <div className="row mb-3">
+                  <div className="col-lg-4 col-md-4 text-md-end text-lg-end ">
+                    <Label htmlFor="participator_name" className="mt-2">
+                      Participator Name
+                    </Label>
+                  </div>
+                  <div className="col-lg-8 col-md-8">
+                    <Input
+                      type="text"
+                      id="participator_name"
+                      name="participator_name"
+                      placeholder=""
+                      onChange={validation.handleChange}
+                      onBlur={validation.handleBlur}
+                      value={validation.values.participator_name || ""}
+                      readOnly
+                    />
                   </div>
                 </div>
-              </CardBody>
-              <CardBody>
-                <TableContainer
-                  columns={columns}
-                  data={participatorList}
-                  isGlobalFilter={true}
-                  customPageSizeOptions={3}
-                  isPagination={true}
-                  iscustomPageSizeOptions={true}
-                  isShowingPageLength={true}
-                  customPageSize={5}
-                  tableClass="table-bordered align-middle nowrap mt-2"
-                  paginationDiv="col-sm-12 col-md-7"
-                  pagination="pagination justify-content-end pagination-rounded"
-                />
-              </CardBody>
-            </Card>
-            <div>
-              <Modal
-                isOpen={open}
-                toggle={() => {
-                  setOpen(false)
-                }}
-              >
-                <div className="modal-header">
-                  <h5 className="modal-title" id="exampleModalLabel">
-                    Add New Participator{" "}
-                  </h5>
-                  <button
-                    type="button"
-                    onClick={() => {
-                      setOpen(false)
-                    }}
-                    className="btn-close"
-                  ></button>
-                </div>
-                <Form>
-                  <div className="modal-body px-5">
-                    <Row className="mb-4">
-                      <Label
-                        htmlFor="horizontal-firstname-Input"
-                        className="col-sm-3 col-form-label"
-                      >
-                        First name
-                      </Label>
-                      <Col sm={9}>
-                        <Input
-                          type="text"
-                          name="first_name"
-                          className="form-control"
-                          id="horizontal-firstname-Input"
-                          placeholder="Enter Your first name"
-                        />
-                      </Col>
-                    </Row>
-                    <Row className="mb-4">
-                      <Label
-                        htmlFor="horizontal-lastname-Input"
-                        className="col-sm-3 col-form-label"
-                      >
-                        Last name
-                      </Label>
-                      <Col sm={9}>
-                        <Input
-                          type="text"
-                          name="last_name"
-                          className="form-control"
-                          id="horizontal-lastname-Input"
-                          placeholder="Enter Your Last Name"
-                        />
-                      </Col>
-                    </Row>
-                    <Row className="mb-4">
-                      <Label
-                        htmlFor="horizontal-email-Input"
-                        className="col-sm-3 col-form-label"
-                      >
-                        Email
-                      </Label>
-                      <Col sm={9}>
-                        <Input
-                          type="email"
-                          name="email"
-                          className="form-control"
-                          id="horizontal-email-Input"
-                          placeholder="Enter Your Email ID"
-                        />
-                      </Col>
-                    </Row>
-                    <Row className="mb-4">
-                      <Label
-                        htmlFor="horizontal-lastname-Input"
-                        className="col-sm-3 col-form-label"
-                      >
-                        Phone
-                      </Label>
-                      <Col sm={9}>
-                        <Input
-                          type="text"
-                          name="phone"
-                          className="form-control"
-                          id="horizontal-lastname-Input"
-                          placeholder="Enter Your Mobile"
-                        />
-                      </Col>
-                    </Row>
-                    <Row className="mb-4">
-                      <Label
-                        htmlFor="horizontal-password-Input"
-                        className="col-sm-3 col-form-label"
-                      >
-                        Password
-                      </Label>
-                      <Col sm={9}>
-                        <Input
-                          type="password"
-                          autoComplete="off"
-                          className="form-control"
-                          id="horizontal-password-Input"
-                          placeholder="Enter Your Password"
-                        />
-                      </Col>
-                    </Row>
-
-                  
+                <div className="row mb-3">
+                  <div className="col-lg-4 col-md-4 text-md-end text-lg-end ">
+                    <Label htmlFor="participator_level" className="mt-2">
+                      Participator level
+                    </Label>
                   </div>
-                  <div className="modal-footer">
+                  <div className="col-lg-8 col-md-8">
+                    <Input
+                      type="text"
+                      id="participator_level"
+                      name="participator_level"
+                      placeholder=""
+                      onChange={validation.handleChange}
+                      onBlur={validation.handleBlur}
+                      value={validation.values.participator_level || ""}
+                      readOnly
+                    />
+                  </div>
+                </div>
+                <div className="row mb-3">
+                  <div className="col-lg-4 col-md-4 text-md-end text-lg-end ">
+                    <Label htmlFor="participator_modality" className="mt-2">
+                      modalities
+                    </Label>
+                  </div>
+                  <div className="col-lg-8 col-md-8">
+                    <Input
+                      type="text"
+                      id="participator_modality"
+                      name="participator_modality"
+                      placeholder=""
+                      onChange={validation.handleChange}
+                      onBlur={validation.handleBlur}
+                      value={validation.values.participator_modality || ""}
+                      readOnly
+                    />
+                  </div>
+                </div>
+                <Col lg={12}>
+                  <div className="text-end">
                     <button
-                      onClick={() => setOpen(false)}
-                      type="button"
-                      className="btn btn-secondary"
-                      data-bs-dismiss="modal"
+                      className="btn btn-outline-secondary"
+                      onClick={() => setModal()}
                     >
-                      Close
-                    </button>
-                    <button type="button" className="btn btn-primary">
-                      + ADD
+                      Cancel
                     </button>
                   </div>
-                </Form>
-              </Modal>
-            </div>
-          </Col>
-        </Card>
-      </Container>
+                </Col>
+              </Row>
+            </Form>
+          </ModalBody>
+        </div>
+      </Modal>
     </React.Fragment>
   )
 }
--- src/pages/Exercise/index.js
+++ src/pages/Exercise/index.js
@@ -35,6 +35,7 @@ const index = () => {
 
     const { Execise, loading } = useSelector(ExeciseDataProperties)
     const [isLoading, setLoading] = useState(loading)
+    
     useEffect(() => {
         const fetchData = () => {
             setLoading(true);
